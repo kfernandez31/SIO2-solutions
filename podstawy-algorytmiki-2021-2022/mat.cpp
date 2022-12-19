48c4a027acb1377f693f7835ee0185c19950a465
kfernandez31 <kacper.fernandez@gmail.com>
2022-12-19 16:23:25 +0100

    finished mat.cpp

--- podstawy-algorytmiki-2021-2022/mat.cpp
+++ podstawy-algorytmiki-2021-2022/mat.cpp
@@ -1,9 +1,7 @@
-//TODO
 #include <bits/stdc++.h>
 using namespace std;
 
-const string FAILURE = "KEEP CALM AND FOLLOW THE WHITE RABBIT";
-const int NONE = 0;
+const string KEEP_CALM = "KEEP CALM AND FOLLOW THE WHITE RABBIT";
 
 const int maxK = 5 * 1e5;
 const int maxN = 5 * 1e5;
@@ -14,17 +12,13 @@ int set_size[maxN + 1];
 bool is_special[maxN + 1];
 bool has_special[maxN + 1];
 pair<int, pair<int, int>> edges[maxM + 1];
-int num_components;
+int num_disconnected;
 int n, m, k;
 
 int find_set(int v) {
-    if (rep[v] == NONE) {
-        return NONE;
-    }
-    if (v == rep[v]) {
-        return v;
-    }
-    return rep[v] = find_set(rep[v]);
+    return (v == rep[v])
+        ? v
+        : rep[v] = find_set(rep[v]);
 }
 
 void union_sets(int x, int y) {
@@ -37,7 +31,7 @@ void union_sets(int x, int y) {
         rep[y] = x;
         set_size[x] += set_size[y];
 
-        num_components -= (has_special[x] && has_special[y]);
+        num_disconnected -= (has_special[x] && has_special[y]);
         has_special[x] |= has_special[y];
     }
 }
@@ -49,22 +43,23 @@ void make_set(int v) {
 }
 
 bool all_connected() {
-    return num_components == 1;
+    return num_disconnected == 1;
 }
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
     cin >> n >> m >> k;
-    while (k--) {
+
+    if (k <= 1) {
+        cout << KEEP_CALM << "\n";
+        return 0;
+    }
+
+    for (int i = 0; i < k; i++) {
         int v;
         cin >> v;
         is_special[v] = true;
-    }
-
-    num_components = k;
-    for (int i = 1; i <= n; i++) {
-        make_set(i);
     }
 
     for (int i = 1; i <= m; i++) {
@@ -72,25 +67,26 @@ int main() {
         cin >> a >> b >> t;
         edges[i] = {t, {a, b}};
     }
-
     sort(edges + 1, edges + m + 1);
 
-    int i = m;
-    while (i >= 1) {
-        int cur_t = edges[i].first;
+    num_disconnected = k;
+    for (int i = 1; i <= n; i++) {
+        make_set(i);
+    }
+
+    int cur_t, i = m;
+    while (i > 0 && !all_connected()) {
+        cur_t = edges[i].first;
 
         while (edges[i].first == cur_t) {
-            auto [a, b] = edges[i].second;
+            int a = edges[i].second.first;
+            int b = edges[i].second.second;
             union_sets(a, b);
             i--;
         }
-
-        if (all_connected()) {
-            cout << i << "\n";
-            return 0;
-        }
-        i++;
     }
-    cout << FAILURE << "\n";
+
+    cout << all_connected() * cur_t - 1 << "\n";
+
     return 0;
 }
